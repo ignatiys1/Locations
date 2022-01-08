e67a3c52620d1432bf2772bc8242ea0e37fb8657
Urban <urbanovich.1@mail.ru>
2022-01-08 23:02:13 +0300

    table

---
+++ Cell.swift
@@ -0,0 +1,23 @@
+//
+//  Cell.swift
+//  Locations
+//
+//  Created by Ignat Urbanovich on 8.01.22.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
